d869c0409eafd119d4b59524034c75adc076fe09
LEGENDcmbk <muhammadayubmahmudov393@gmail.com>
2024-05-15 19:47:46 +0500

    text edit

--- src/pages/buyurtmalar/index.tsx
+++ src/pages/buyurtmalar/index.tsx
@@ -3,15 +3,15 @@ import "./style.scss"
 const index = () => {
     return <>
     <div className="">
-         <h1 className="text-[24px] font-bold ">Buyurtmalar</h1>
-         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-          dignissimos repellat illohhh necessitatibam dicta odit itaque
-          eveniet, eos incidunt iste. Quasi nam assumenda provident
-          necessitatibus corporis ducimus quia deleniti qui? Ipsum adipisci,
-          dolor qui repellendus iure ut quasi ipsa harum earum corrupti totam
-          pariatur et minima voluptas eius dolore.
-        </p>
+            <h1 className="text-[24px] font-bold ">Buyurtmalar</h1>
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+            dignissimos repellat illohhh necessitatibam dicta odit itaque
+            eveniet, eos incidunt iste. Quasi nam assumenda provident
+            necessitatibus corporis ducimus quia deleniti qui? Ipsum adipisci,
+            dolor qui repellendus iure ut quasi ipsa harum earum corrupti totam
+            pariatur et minima voluptas eius dolors.
+            </p>
         <p>Vercel test </p>
     </div>
     </>
